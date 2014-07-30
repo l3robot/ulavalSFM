08f5ff111937ab504cacf05eae2d52dedbfbbce5
LERobot <erobotaille@gmail.com>
2014-07-30 14:26:25 -0400

    install.sh is up to date

--- install.sh
+++ install.sh
@@ -9,13 +9,16 @@
 echo "#Added by ulavalSFM" >> $HOME/.bashrc
 echo "export LD_LIBRARY_PATH="$PWD/lib/:$LD_LIBRARY_PATH"" >> $HOME/.bashrc
 echo "export PATH="$PWD/bin/:$PATH"" >> $HOME/.bashrc
+echo "export PATH="$PWD/bundler_sfm/bin/:$PATH"" >> $HOME/.bashrc
 export LD_LIBRARY_PATH="$PWD/lib/:$LD_LIBRARY_PATH"
 export PATH="$PWD/bin/:$PATH"
-echo "[ Adding library path in .bashrc file ... done ]"
+echo "[ Adding library and executable path in .bashrc file ... done ]"
 
 cd lib/
 git clone https://github.com/Itseez/opencv
 echo "[ Cloning OpenCV ... done ]"
+git clone https://github.com/LERobot/bundler_sfm
+echo "[ Cloning bundler_sfm ... done ]"
 
 cd opencv/
 mkdir build/
@@ -23,6 +26,12 @@ cd build/
 cmake -D CMAKE_INSTALL_PREFIX="../../../" -D CMAKE_LIBRARY_PATH="../../" -D CMAKE_INCLUDE_PATH="../../../include/" ..
 make -j 2
 make install
+echo "[ Building OpenCV ... done ]"
+
+cd ../bundler_sfm/
+mkdir build/
+make clean
+make -j 2
 echo "[ Building OpenCV ... done ]"
 
 cd ../../../src/
