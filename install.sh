1eea104c8d12a3ee1945d7c15be9a308c01fc4a3
Émile Robitaille <emile.courriel@gmail.com>
2014-10-05 18:43:17 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,8 @@
 # Version : 1.0
 # Description : Intall script if you don't have OpenCV. Linux version
 
-N="2"
+N="1"
+OUT="$(PWD)/out.txt"
 
 #echo "#Added by ulavalSFM" >> $HOME/.bashrc
 #echo "export LD_LIBRARY_PATH="$PWD/lib/:$LD_LIBRARY_PATH"" >> $HOME/.bashrc
@@ -14,17 +15,17 @@ export LD_LIBRARY_PATH="$PWD/lib/:$LD_LIBRARY_PATH"
 echo "[ Adding library path in .bashrc file ... done ]"
 
 cd lib/
-git clone https://github.com/Itseez/opencv >> out.txt 2>&1
+git clone https://github.com/Itseez/opencv >> OUT 2>&1
 if [ $? -eq 0 ]; then
 	echo "[ ERROR : cloning OpenCV, check out.txt for more information ]"
-	exit
+	return
 else
 	echo "[ Cloning OpenCV ... done ]"
 fi
-git clone https://github.com/LERobot/bundler_sfm >> out.txt 2>&1
+git clone https://github.com/LERobot/bundler_sfm >> OUT 2>&1
 if [ $? -eq 0 ]; then
 	echo "[ ERROR : Cloning BundlerSFM, check out.txt for more information ]"
-	exit
+	return
 else
 	echo "[ Cloning BundlerSFM ... done ]"
 fi
@@ -38,52 +39,52 @@ else
 fi
 cd build/
 cmake -D CMAKE_INSTALL_PREFIX="../../../" -D CMAKE_LIBRARY_PATH="../../" -D CMAKE_INCLUDE_PATH="../../../include/" .. >> out.txt 2>&1
-make -j $(N) >> out.txt 2>&1
+make -j $(N) >> OUT 2>&1
 if [ $? -eq 0 ]; then
 	echo "[ ERROR : Building OpenCV, check out.txt for more information ]"
-	exit
+	return
 else
 	echo "[ Building OpenCV ... done ]"
 fi
-make install >> out.txt 2>&1
+make install >> OUT 2>&1
 if [ $? -eq 0 ]; then
 	echo "[ ERROR : Installing OpenCV, check out.txt for more information ]"
-	exit
+	return
 else
 	echo "[ Installing OpenCV ... done ]"
 fi
 
 cd ../../bundler_sfm/
-make clean >> out.txt 2>&1
-make -j 2 >> out.txt 2>&1
+make clean >> OUT 2>&1
+make -j 2 >> OUT 2>&1
 if [ $? -eq 0 ]; then
 	echo "[ ERROR : Building BundlerSFM, check out.txt for more information ]"
-	exit
+	return
 else
 	echo "[ Building BundlerSFM ... done ]"
 fi
-mv bin/* ../../bin/ >> out.txt 2>&1
-mv lib/* ../../lib/ >> out.txt 2>&1
+mv bin/* ../../bin/ >> OUT 2>&1
+mv lib/* ../../lib/ >> OUT 2>&1
 if [ $? -eq 0 ]; then
 	echo "[ ERROR : Installing BundlerSFM, check out.txt for more information ]"
-	exit
+	return
 else
 	echo "[ Installing BundlerSFM ... done ]"
 fi
 
 cd ../../src/
-make clean >> out.txt 2>&1
-make -j 2 out.txt >> 2>&1
+make clean >> OUT 2>&1
+make -j 2 OUT >> 2>&1
 if [ $? -eq 0 ]; then
 	echo "[ ERROR : Building ulavalSFM, check out.txt for more information ]"
-	exit
+	return
 else
 	echo "[ Building ulavalSFM ... done ]"
 fi
-make install out.txt >> 2>&1
+make install OUT >> 2>&1
 if [ $? -eq 0 ]; then
 	echo "[ ERROR : Installing ulavalSFM, check out.txt for more information ]"
-	exit
+	return
 else
 	echo "[ Installing ulavalSFM ... done ]"
 fi
