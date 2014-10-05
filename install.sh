dda9ce73ae5ac97ac085dc55d889f89076236700
Émile Robitaille <emile.courriel@gmail.com>
2014-10-05 18:26:32 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@
 
 N="2"
 
-echo "#Added by ulavalSFM" >> $HOME/.bashrc
-echo "export LD_LIBRARY_PATH="$PWD/lib/:$LD_LIBRARY_PATH"" >> $HOME/.bashrc
+#echo "#Added by ulavalSFM" >> $HOME/.bashrc
+#echo "export LD_LIBRARY_PATH="$PWD/lib/:$LD_LIBRARY_PATH"" >> $HOME/.bashrc
 export LD_LIBRARY_PATH="$PWD/lib/:$LD_LIBRARY_PATH"
 echo "[ Adding library path in .bashrc file ... done ]"
 
@@ -17,12 +17,14 @@ cd lib/
 git clone https://github.com/Itseez/opencv >> out.txt 2>&1
 if [ $? -eq 0 ]; then
 	echo "[ ERROR : cloning OpenCV, check out.txt for more information ]"
+	exit
 else
 	echo "[ Cloning OpenCV ... done ]"
 fi
 git clone https://github.com/LERobot/bundler_sfm >> out.txt 2>&1
 if [ $? -eq 0 ]; then
 	echo "[ ERROR : Cloning BundlerSFM, check out.txt for more information ]"
+	exit
 else
 	echo "[ Cloning BundlerSFM ... done ]"
 fi
@@ -39,12 +41,14 @@ cmake -D CMAKE_INSTALL_PREFIX="../../../" -D CMAKE_LIBRARY_PATH="../../" -D CMAK
 make -j $(N) >> out.txt 2>&1
 if [ $? -eq 0 ]; then
 	echo "[ ERROR : Building OpenCV, check out.txt for more information ]"
+	exit
 else
 	echo "[ Building OpenCV ... done ]"
 fi
 make install >> out.txt 2>&1
 if [ $? -eq 0 ]; then
 	echo "[ ERROR : Installing OpenCV, check out.txt for more information ]"
+	exit
 else
 	echo "[ Installing OpenCV ... done ]"
 fi
@@ -54,6 +58,7 @@ make clean >> out.txt 2>&1
 make -j 2 >> out.txt 2>&1
 if [ $? -eq 0 ]; then
 	echo "[ ERROR : Building BundlerSFM, check out.txt for more information ]"
+	exit
 else
 	echo "[ Building BundlerSFM ... done ]"
 fi
@@ -61,6 +66,7 @@ mv bin/* ../../bin/ >> out.txt 2>&1
 mv lib/* ../../lib/ >> out.txt 2>&1
 if [ $? -eq 0 ]; then
 	echo "[ ERROR : Installing BundlerSFM, check out.txt for more information ]"
+	exit
 else
 	echo "[ Installing BundlerSFM ... done ]"
 fi
@@ -70,12 +76,14 @@ make clean >> out.txt 2>&1
 make -j 2 out.txt >> 2>&1
 if [ $? -eq 0 ]; then
 	echo "[ ERROR : Building ulavalSFM, check out.txt for more information ]"
+	exit
 else
 	echo "[ Building ulavalSFM ... done ]"
 fi
 make install out.txt >> 2>&1
 if [ $? -eq 0 ]; then
 	echo "[ ERROR : Installing ulavalSFM, check out.txt for more information ]"
+	exit
 else
 	echo "[ Installing ulavalSFM ... done ]"
 fi
