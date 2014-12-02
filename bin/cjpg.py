247cd20854ddfb169d972fa2588469fec583ec11
Émile Robitaille <emile.courriel@gmail.com>
2014-12-01 21:31:49 -0500

    Update cjpg.py

--- bin/cjpg.py
+++ bin/cjpg.py
@@ -6,16 +6,21 @@
 
 import os
  
-a = input("Enter the directory name here : ");
+a = input("Enter the directory name here : ")
+
+if a[len(a)-1] != '/':
+	a = a + '/'
  
 b = os.listdir(a)
  
 for i in b:
-	c = a + i
-	f = open(c, "rb")
-	image = f.read()
-	if image[0] == 0x89 and image[1] == 0x50:
-		print("Will transform " + i + " in PNG, because it's a PNG file...")
-		name = i[0:-4] + ".png"
-		command = "mv " + a + i + " " + a + name
-		os.system(command)
+	ext = i[len(i)-3:len(i)-1]
+	if ext == "jpg":
+		c = a + i
+		f = open(c, "rb")
+		image = f.read()
+		if image[0] == 0x89 and image[1] == 0x50:
+			print("Will transform " + i + " in PNG, because it's a PNG file...")
+			name = i[0:-4] + ".png"
+			command = "mv " + a + i + " " + a + name
+			os.system(command)
