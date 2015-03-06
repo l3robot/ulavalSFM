d1f5934881b1c534d449cc2834e4415745094e9f
Louis-Émile Robitaille @ L3Robot <emile.courriel@gmail.com>
2015-03-06 16:32:28 -0500

    inital commit on clean up branch

--- src/dosift.cpp
+++ src/dosift.cpp
@@ -1,10 +1,10 @@
 /*
-*	File : dosift.cpp
+*	File : cDoSift.cpp
 *	Author : Émile Robitaille @ LERobot
-*	Creation date : 2014, June 27th
+*	Creation date : 2014, June 30th
 *	Version : 1.0
 *	
-*	Description : Functions relative to sift
+*	Description : Program to find sift point on a image dataset in parallel
 */
 
 #include <stdio.h>
@@ -12,100 +12,65 @@
 #include <math.h>
 #include <string.h>
 #include <iostream>
-#include <string>
-#include <vector>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/core/core.hpp>
-#include <opencv2/opencv.hpp>
-#include <opencv2/nonfree/nonfree.hpp>
-#include <opencv2/features2d/features2d.hpp>
+#include <mpi.h>
 
 #include "directory.h"
-#include "dosift.h"
 #include "util.h"
+#include "libsift.h"
 
 using namespace std;
-using namespace cv;
+using namespace util;
 
 
-/* 
-*	Function : doSift
-*	Description : Find sift points on the image
-*	
-*	path : path of the image
-*	container : container for sift keypoints and their descriptor
-*/
-void doSift(const string &path, struct SFeatures &container)
+
+int main(int argc, char* argv[])
 {
-	Mat img, des;
-	vector<KeyPoint> keypoints;
+	MPI_Init(&argc, &argv);
 
-	img = imread(path.c_str(), CV_LOAD_IMAGE_GRAYSCALE);
-
-	SiftFeatureDetector detector; 
-
-   	detector.detect(img, keypoints);
-
-   	SiftDescriptorExtractor extractor;
-
-    extractor.compute(img, keypoints, des);
-
-    container.des = des;
-    container.keys = keypoints;
-}
-
-/* 
-*	Function : WriteSiftFile
-*	Description : write the keypoints and their descriptor in the sift files (Lowe's binairy format)
-*	
-*	file : path of the .key file
-*	container : container for sift keypoints and their descriptor
-*/
-void writeSiftFile(const string &file, const struct SFeatures &container)
-{
-	FILE* f = fopen(file.c_str(), "wb");
-
-    fprintf(f, "%d %d \n", container.des.rows, container.des.cols);
-
-    for(int i = 0; i < container.keys.size(); i++)
-    {
-      	fprintf(f, "%f %f %f %f \n", container.keys.at(i).pt.y, container.keys.at(i).pt.x, container.keys.at(i).size, (container.keys.at(i).angle*M_PI/180.0));
-       	for(int j = 0; j < 128; j++)
-       	{
-       	fprintf(f, "%d ", (int)container.des.at<float>(i,j));
-       	if ((j + 1) % 19 == 0) fprintf(f, "\n");
-       	}
-       	fprintf(f, "\n");
-    }
-
-    fclose(f);
-}
-
-/* 
-*	Function : sift1Core
-*	Description : Find sift points and write those in files
-*	
-*	dir : directory information
-*/
-void sift1Core(const util::Directory &dir)
-{
+	int* dist = NULL;
+	int recv[2];
 	double the_time;
+
+	int netSize;
+	int netID;
+
+	MPI_Comm_size(MPI_COMM_WORLD, &netSize);
+	MPI_Comm_rank(MPI_COMM_WORLD, &netID);
+
+	util::Directory dir(argv[1]);
 	struct SFeatures container;
-	string file(dir.getPath());
+	string file(argv[1]);
 
-	cout << endl;
+	if(netID == 0)
+	{
+		dist = createDist(dir, netSize);
+	}
 
-	int n = dir.getNBImages();
+	MPI_Scatter(dist, 2, MPI_INT, recv, 2, MPI_INT, 0, MPI_COMM_WORLD);
 
-	printf("--> Sift searching begins : \n");
+	// SIFT
 
-	for(int i = 0; i < n; i++)
+	if(netID == 0)
+	{
+		deleteDist(dist);
+		the_time = MPI_Wtime();
+	}
+
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	int start = recv[0];
+	int end = recv[1];
+
+	if (netID == 0)
+		printf("--> Sift searching begins on %d core(s) :\n", netSize);
+
+	for(int i = start; i < end; i++)
 	{
 		file.append(dir.getImage(i));
 
 		doSift(file, container);
 
-		//cout << container.keys.size() << " sift point(s) found in " << dir.getImage(i) << endl;
+		//cout << "[CORE " << netID << "]: " << container.keys.size() << " sift point(s) found in " << dir.getImage(i) << endl;
 
 		while (file[file.size() - 1] != '.')
 		{
@@ -121,56 +86,32 @@ void sift1Core(const util::Directory &dir)
 			file.pop_back();
 		}
 
-		showProgress(i, n, 75, 1);
+		if (netID == 0)
+			showProgress(i, end, 75, 1);
+	} 
+
+	if (netID == 0)
+		showProgress(end, end, 75, 0);
+
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	if(netID == 0)
+	{
+		printf("The program takes approximately %f second(s)\n", MPI_Wtime() - the_time);
 	}
 
-	showProgress(n, n, 75, 0);
+	MPI_Finalize();
 
-	cout << endl;
+	return 0;
 }
 
-/* 
-*	Function : siftMCore
-*	Description : start to find sift point with OpenMPI on the given number of cores
-*	
-*	path : working directory
-*	numcore : number of cores
-*/
-void siftMCore(const string &path, int numcore)
-{
-	stringstream c;
 
-	c << "mpirun -n " << numcore << " cDoSift " << path;
 
-	string command = c.str();
 
-	system(command.c_str());
-}
 
-/* 
-*	Function : siftCMCore
-*	Description : start to find sift point with OpenMPI on the given number of cores and on the supercomputer
-*	
-*	path : working directory
-*	numcore : number of cores
-*/
-void siftMCCore(const string &path, int numcore, int seconds)
-{
-	stringstream c;
 
-	printf("--> Create the script : \n");
 
-	createSubmit(path, numcore, seconds, 0);
 
-	printf("--> Launch the script : \n");
 
-	c << "msub ulavalSub/submit.sh";
-
-	string command = c.str();
-
-	system(command.c_str());
-
-	cout << "Process launch, you can enter the command \"watch -n 10 showq -u $USER\" to see the progression." << endl << endl;
-}
 
 
--- src/util.h
+++ src/util.h
@@ -12,46 +12,6 @@
 
 #include "directory.h"
 
-#define VERSION "3.0"
-
-#define BTOS(x) ((x)?"supercomputer":"computer")
-
-/* 
-*	Struct : Opts
-*	Description : Configuration struct fill after parsing
-*	
-*	int choice : choice of algorithm(s)
-*	unsigned int cores : number of cores to work on
-*	bool cluster : on cluster or not
-*	char* dir : working directory
-*/
-struct Opts
-{
-	int choice;
-	int cores;
-	int seconds;
-	int geo;
-	bool cluster;
-	util::Directory dir;
-
-	//default constructor
-	Opts()
-	{
-		choice = 0;
-		cores = 1;
-		seconds = 300;
-		geo = 1;
-		cluster = false;
-	}
-};
-
-//Command line functions
-void parseArgs(int argc, char* argv[], Opts &o);
-void printVersion();
-void printHelp();
-void printError();
-void listDir(const util::Directory &dir);
-
 //MPI distribution functions
 int* createDist(const util::Directory &dir, int netSize);
 void deleteDist(int* dist);
